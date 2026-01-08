7be5dad868be9cde29d7ab94be5683ec85452534
Vishwa <vishwalinga7545@gmail.com>
2026-01-08 17:32:31 +0530

    Add file upload and data preview

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title="Marketing Strategy AI Copilot")
 
 st.title("🚀 Marketing Strategy AI Copilot")
-st.write("This is my first working AI project.")
+st.write("Upload your marketing data (Excel or CSV)")
+
+uploaded_file = st.file_uploader(
+    "Upload marketing data file",
+    type=["xlsx", "csv"]
+)
+##DISPLAY DATA
+
+if uploaded_file is not None:
+    if uploaded_file.name.endswith(".csv"):
+        df = pd.read_csv(uploaded_file)
+    else:
+        df = pd.read_excel(uploaded_file)
+
+    st.subheader("📊 Uploaded Data Preview")
+    st.dataframe(df.head())
